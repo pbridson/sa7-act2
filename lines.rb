41f91591a2ee623b4120964058f54385bb3feae9
Peter Bridson <pbridson@memphis.edu>
2024-02-28 16:04:06 -0600

    Add answer file for 12

--- lines.rb
+++ lines.rb
@@ -1,6 +1,10 @@
-File.open('sample.txt' 'r') do |f|
+File.open('sample.txt', 'r') do |f|
   lines = f.readlines
-  lines.each do |line|
-    puts "Line : #{line}"
+  # puts "Line 1: #{lines[0]}"
+  # puts "Line 2: #{lines[1]}"
+  # puts "Line 3: #{lines[2]}"
+  lines.each_with_index do |line, idx|
+    linenumber = idx + 1
+    puts "Line #{linenumber}: #{line}" if linenumber < 4
   end
 end
